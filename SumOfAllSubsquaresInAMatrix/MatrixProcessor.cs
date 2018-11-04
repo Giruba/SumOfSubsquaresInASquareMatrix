bf8ebeef4b394affef141c16daeada4f3c52efc3
gibeulah <gibeulah@microsoft.com>
2018-11-04 18:37:12 +0530

    First Commit

---
+++ SumOfAllSubsquaresInAMatrix/MatrixProcessor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SumOfAllSubsquaresInAMatrix
+{
+    class MatrixProcessor
+    {
+        int[,] matrix;
+
+        public MatrixProcessor(int[,] matrix) {
+            this.matrix = matrix;
+        }
+
+        public void GetSumOfSubsquaresInSquareMatrix(int subSquareSize) {
+
+            Console.WriteLine("-----------The Input Matrix---------------------");
+            PrintMatrix(matrix);
+
+            int dimension = matrix.GetLength(0);
+            int[,] subSquareMatrix = new int[subSquareSize, subSquareSize];
+
+            for (int i = 0; i < subSquareSize; i++) {
+                for (int j = 0; j < subSquareSize; j++) {
+                    subSquareMatrix[i, j] = GetSubSquareMatrixSum(i,j,subSquareSize);
+                }
+            }
+
+            Console.WriteLine("-------------The SubSquare Sum Matrix-----------");
+            PrintMatrix(subSquareMatrix);
+        }
+
+        private int GetSubSquareMatrixSum(int i, int j, int SubSquareSize) {
+
+            int subSquareSum = 0;
+
+            int rows = i + SubSquareSize;
+            int columns = j + SubSquareSize;
+
+            for (int index = i; index < rows; index++) {
+                for (int secondIndex = j; secondIndex < columns; secondIndex++) {
+                    subSquareSum += matrix[index, secondIndex];
+                }
+            }
+
+            return subSquareSum;
+        }
+
+
+        private void PrintMatrix(int[,] incomingMatrix) {
+
+            for (int i = 0; i < incomingMatrix.GetLength(0); i++) {
+                for (int j = 0; j < incomingMatrix.GetLength(1); j++) {
+                    Console.Write(incomingMatrix[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("--------------------------------------");
+        }
+    }
+}
--- SumOfAllSubsquaresInAMatrix/Program.cs
+++ SumOfAllSubsquaresInAMatrix/Program.cs
@@ -6,7 +6,46 @@ namespace SumOfAllSubsquaresInAMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sum of all sub-squares of size K in a Square matrix!");
+            Console.WriteLine("----------------------------------------------------");
+
+            int[,] squareMatrix = GetMatrix();
+            Console.WriteLine("Enter the sub-square matrix size");
+            try
+            {
+                int subSquareSize = int.Parse(Console.ReadLine());
+                MatrixProcessor matrixProcessor = new MatrixProcessor(squareMatrix);
+                matrixProcessor.GetSumOfSubsquaresInSquareMatrix(subSquareSize);
+            }
+            catch (Exception exception) {
+                Console.WriteLine("The thrown exception is"+exception.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int[,] GetMatrix() {
+            int[,] squareMatrix = null;
+            Console.WriteLine("Enter the dimension of the square matrix");
+            try
+            {
+                int dimension = int.Parse(Console.ReadLine());
+                squareMatrix = new int[dimension, dimension];
+                for (int i = 0; i < dimension; i++)
+                {                    
+                    Console.WriteLine("Enter the elements of the row:" + i + "" +
+                        " separated by space");
+                    String allElementString = Console.ReadLine();
+                    String[] elements = allElementString.Split(' ');
+                    for (int j = 0; j < elements.Length; j++) {
+                        squareMatrix[i, j] = int.Parse(elements[j]);
+                    }
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("The thrown exception is"+exception.Message);
+            }
+            return squareMatrix;
         }
     }
 }
